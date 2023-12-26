e4730e92955f5f557a7c171725591380c85d63c2
JongHoon <qnm541@icloud.com>
2023-12-26 10:18:29 +0900

    Initial Commit

---
+++ Sesac3WebSocket/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sesac3WebSocket
+//
+//  Created by JongHoon on 12/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sesac3WebSocket/Sesac3WebSocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sesac3WebSocketApp.swift
+//  Sesac3WebSocket
+//
+//  Created by JongHoon on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Sesac3WebSocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
